6daf3ee88b24e173d86d34d6330151ffde730586
ThroughTheThornsToTheStarss <ilshat.nas@mail.ru>
2024-09-23 21:58:07 +0300

    B: screen 60 FPS in sec

--- main.py
+++ main.py
@@ -4,18 +4,18 @@
 import pygame
 from constants import *
 pygame.init()
+clock = pygame.time.Clock()
+dt = 0 
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 from pygame.color import THECOLORS
 def main() :
-    print("Starting asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill(THECOLORS['black'])
         pygame.display.flip()
+        dt = clock.tick(60)/1000
 
 if __name__ == "__main__":
     main()
