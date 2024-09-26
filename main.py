42b3e8946b3c361f7612afa17e2a4410b3fc9cc2
ThroughTheThornsToTheStarss <ilshat.nas@mail.ru>
2024-09-26 22:02:05 +0300

    D: finish

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import pygame
 from asteroidfield import AsteroidField
 from constants import *
 from player import Player
+from shot import Shot
 from asteroid import *
 from pygame.color import THECOLORS
 
@@ -9,44 +10,57 @@ def main() :
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
-    
+    font = pygame.font.Font(None, 36)
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
     asteroids = pygame.sprite.Group()
+    bullets = pygame.sprite.Group()
 
     Player.containers = (updatable, drawable) 
     Asteroid.containers = (asteroids, updatable, drawable)
     AsteroidField.containers = (updatable,)
+    Shot.containers = (bullets, updatable, drawable) 
    
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     asteroid_field = AsteroidField()
 
     dt = 0 
-
+    score = 0
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-            
-        for obj in updatable:
-            obj.update(dt)
-
-        screen.fill(THECOLORS['black'])
-
-        for obj in drawable:
-            obj.draw(screen)
-
         
-        pygame.display.flip()
-        
-       
         for obj in asteroids:
             if obj.colliding(player):
                 print("Game over!")
                 return
 
-        dt = clock.tick(60)/1000
+        for obj in updatable:
+            obj.update(dt)
+           
+
+        screen.fill(THECOLORS['black'])
+
+        for obj in drawable:
+            obj.draw(screen)
         
+        for asteroid in asteroids:
+            for  bullet in bullets:
+                if asteroid.colliding( bullet):
+                    points_earned = asteroid.split()
+                    score += points_earned
+                    bullet.kill()
+
+        score_text = font.render(f"Score: {score}", True, THECOLORS['white'])
+        screen.blit(score_text, (10, 10))
+        pygame.display.flip()
+        
+       
+      
+
+        dt = clock.tick(60)/1000
+       
 
        
 if __name__ == "__main__":
--- player.py
+++ player.py
@@ -1,13 +1,14 @@
 import pygame
 from circleshape import CircleShape
 from constants import *
-
+from shot import Shot
 class Player(CircleShape):
     
     def __init__(self, x, y):
         super().__init__(x, y, PLAYER_RADIUS)  
         self.rotation = 0
-
+        self.clock = 0
+        
     def triangle(self):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
         right = pygame.Vector2(0, 1).rotate(self.rotation + 90) * self.radius / 1.5
@@ -22,24 +23,33 @@ class Player(CircleShape):
     def rotate(self, dt):
         self.rotation += PLAYER_TURN_SPEED * dt
 
+    
+
     def update(self, dt):
         keys = pygame.key.get_pressed()
-
-        if keys[pygame.K_a]:
+        self.clock -= dt
+        if keys[pygame.K_a] or keys[pygame.K_LEFT]:
             self.rotate(-dt)
-        if keys[pygame.K_d]:
+        if keys[pygame.K_d]or keys[pygame.K_RIGHT]:
             self.rotate(dt)
-        if keys[pygame.K_w]:
+        if keys[pygame.K_w]or keys[pygame.K_UP]:
             self.move(dt)
-        if keys[pygame.K_s]:
-            self.move(-dt)        
+        if keys[pygame.K_s]or keys[pygame.K_DOWN]:
+            self.move(-dt)
+        if keys[pygame.K_SPACE]:
+            if self.clock <= 0:
+                self.shoot()
+                self.clock = 0.3
+          
 
     def move(self, dt):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
         self.position += forward * PLAYER_SPEED * dt
 
 
-
+    def shoot(self):
+        bullet = Shot(self.position.x, self.position.y)
+        bullet.velocity = pygame.Vector2(0, 1).rotate(self.rotation) * PLAYER_SHOOT_SPEED
 
 
 
